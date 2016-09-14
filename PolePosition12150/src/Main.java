8fb9531abd57eb5d3b6d25d8c09395a0906afbe2
nmd001 <nmd001@lyn188-16>
2016-09-14 15:17:51 -0400

    cps update, project done

--- PolePosition12150/src/Main.java
+++ PolePosition12150/src/Main.java
@@ -1,39 +1,58 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
+	public static class PolePosition {
+
+		int number, position;
+
+		public PolePosition(int number, int position) {
+			this.number = number;
+			this.position = position;
+		}
+
+		public int getPosition() {
+			return position;
+		}
+
+		public int getNumber() {
+			return number;
+		}
+	}
+
 	public static void main(String[] args) {
-		//create a scanner to read the input
+		// create a scanner to read the input
 		Scanner reader = new Scanner(System.in);
-		
-		//loop to read all inputs into an ArrayList 
-		//breaks when the EOF character is put in
-		while (true){
+
+		// loop to read all inputs into an ArrayList
+		// breaks when the EOF character is put in
+		while (true) {
 			int x = reader.nextInt();
-			if(x == 0) break;
-			else{
+			if (x == 0)
+				break;
+			else {
 				String answer = "";
 				PolePosition[] startingGrid = new PolePosition[x];
-				for(int count = 0; count < x; count++){
+				for (int count = 0; count < x; count++) {
 					PolePosition temp = new PolePosition(reader.nextInt(), reader.nextInt());
-					try{
-						if(startingGrid[count + temp.getPosition()] == null){
+					try {
+						if (startingGrid[count + temp.getPosition()] == null) {
 							startingGrid[count + temp.getPosition()] = temp;
-						}
-						else
+						} else
 							throw new Exception("Throw me");
-					} catch(Exception e) {
+					} catch (Exception e) {
 						answer = "-1";
-						break;
 					}
 				}
-				
-				if(answer.equals("-1"))
+
+				if (answer.equals("-1"))
 					System.out.println(answer);
-				else{
-					for(int i = 0; i < x; i++){
-						answer += startingGrid[i].getNumber() + " ";
+				else {
+					
+					answer += "" + startingGrid[0].getNumber();
+					
+					for (int i = 1; i < x; i++) {
+						answer += " " + startingGrid[i].getNumber();
 					}
 					System.out.println(answer);
 				}
