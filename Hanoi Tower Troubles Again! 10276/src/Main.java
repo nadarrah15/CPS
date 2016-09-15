7825ebafdb2bb3b60f08e0b6498aad6fd45406ac
nmd001 <nmd001@lyn188-07>
2016-09-15 13:14:55 -0400

    gumption

--- Hanoi Tower Troubles Again! 10276/src/Main.java
+++ Hanoi Tower Troubles Again! 10276/src/Main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		int[][] array;
 		
+		int count = reader.nextInt();
+		for(int i = 0; i < count; i++){
+			
+		}
 	}
 
 }
