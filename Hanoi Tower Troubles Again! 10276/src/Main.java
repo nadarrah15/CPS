eadc2537ced0561b60b969f190bfd5863615fdbb
Nathan <nadarrah15@gmail.com>
2016-09-19 13:18:09 -0400

    finish 2

--- Hanoi Tower Troubles Again! 10276/src/Main.java
+++ Hanoi Tower Troubles Again! 10276/src/Main.java
@@ -1,15 +1,45 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
+	static int[] array;
+	static int size;
+	static int total;
+
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
-		int[][] array;
-		
+
 		int count = reader.nextInt();
-		for(int i = 0; i < count; i++){
-			
+		for (int i = 0; i < count; i++) {
+			size = reader.nextInt();
+			array = new int[size];
+			Arrays.fill(array, 0, array.length, 0);
+			total = 0;
+
+			System.out.println(backtrack(1, 0));
 		}
 	}
 
+	static int backtrack(int value, int position) {
+		if (position == size)
+			return total;
+		else if (array[position] == 0) {
+			array[position] = value;
+			total++;
+			return backtrack(value + 1, position);
+		} 
+		else {
+			for (int i = 0; i <= position; i++) {
+				int root = (int) Math.sqrt(array[i] + value);
+				if (root * root == array[i] + value) {
+					array[i] = value;
+					total++;
+					return backtrack(value + 1, position);
+				}
+			}
+		}
+
+		return backtrack(value, position + 1);
+	}
 }
