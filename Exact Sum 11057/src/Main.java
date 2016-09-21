cb317ea3bbfc93c782936768c2fd9ed9d20b1155
Nathan <nadarrah15@gmail.com>
2016-09-21 13:38:27 -0400

    cps update

--- Exact Sum 11057/src/Main.java
+++ Exact Sum 11057/src/Main.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 
 public class Main {
 
+	static int N;
+	static int[] array;
+	
 	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
 		
+		N = reader.nextInt();
+		array = new int[N];
+		
+		for(int i = 0; i < N; i++){
+			array[i] = reader.nextInt();
+		}
 	}
 
 }
