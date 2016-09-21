b80174fc5be54cba3eec8e27bf91eaee81632f7b
nmd001 <nmd001@lyn188-16>
2016-09-21 16:17:05 -0400

    cps update

--- Exact Sum 11057/src/Main.java
+++ Exact Sum 11057/src/Main.java
@@ -1,20 +1,43 @@
-//Binary search M - price[i]
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
-	static int N;
-	static int[] array;
-	
+		
 	public static void main(String[] args) {
+
 		Scanner reader = new Scanner(System.in);
-		
-		N = reader.nextInt();
-		array = new int[N];
-		
-		for(int i = 0; i < N; i++){
-			array[i] = reader.nextInt();
+		while(reader.hasNext()){
+			
+			int N = reader.nextInt();
+			int[] array = new int[N];
+			
+			for(int i = 0; i < N; i++){
+				array[i] = reader.nextInt();
+			}
+			
+			int M = reader.nextInt();
+			
+			
+			Arrays.sort(array);
+			int answerOne = 0;
+			int answerTwo = 0;
+			int i = 0;
+			int j = array.length - 1;
+			while(i < j){
+				if(array[i] + array[j] > M){
+					j++;
+				}
+				else if(array[i] + array[j] < M){
+					i++;
+				}
+				else {
+					answerOne = array[i];
+					answerTwo = array[j];
+				}
+			}
+			
+			System.out.println("Peter should buy books whose prices are " + answerOne + " and " + answerTwo);
+			System.out.println("");
 		}
 	}
-
 }
