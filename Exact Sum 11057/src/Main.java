12a41959de10bf2660cd6f918ef4cbc7d5dbce71
Nathan <nadarrah15@gmail.com>
2016-09-26 12:37:56 -0400

    g

--- Exact Sum 11057/src/Main.java
+++ Exact Sum 11057/src/Main.java
@@ -2,9 +2,11 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-		
-	public static void main(String[] args) {
 
+	static int M;
+
+	public static void main(String[] args) {
+	
 		Scanner reader = new Scanner(System.in);
 		while(reader.hasNext()){
 			
@@ -15,29 +17,44 @@ public class Main {
 				array[i] = reader.nextInt();
 			}
 			
-			int M = reader.nextInt();
-			
-			
-			Arrays.sort(array);
+			M = reader.nextInt();
+			reader.nextLine();
+			reader.nextLine();
 			int answerOne = 0;
 			int answerTwo = 0;
-			int i = 0;
-			int j = array.length - 1;
-			while(i < j){
-				if(array[i] + array[j] > M){
-					j = (i + j) / 2;
-				}
-				else if(array[i] + array[j] < M){
-					i = (i + j) / 2;
-				}
-				else {
-					answerOne = array[i];
-					answerTwo = array[j];
-				}
+			Arrays.sort(array);
+			for(int i = N / 2; i >= 0; i--){
+				answerOne = array[i];
+				answerTwo = binarySearch(array, array[i]);
+				
+				if(answerTwo != 0 && answerTwo != -1) break;
 			}
 			
-			System.out.println("Peter should buy books whose prices are " + answerOne + " and " + answerTwo);
-			System.out.println("");
+			if(answerOne < answerTwo){
+				System.out.println("Peter should buy books whose prices are " + answerOne + " and " + answerTwo + ".");
+				System.out.println("");
+			}
+			else{
+				System.out.println("Peter should buy books whose prices are " + answerTwo + " and " + answerOne + ".");
+				System.out.println("");
+			}
 		}
+	}
+
+	public static int binarySearch(int[] array, int num) {
+		int low = 0;
+		int high = array.length - 1;
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			if (M - num == array[mid]) {
+				return array[mid];
+			} 
+			else if (M - num > array[mid])
+				low = mid + 1;
+			else if (M - num < array[mid])
+				high = mid - 1;
+		}
+
+		return -1;
 	}
 }
