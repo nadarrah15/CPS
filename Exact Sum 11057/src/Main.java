5cb5586a1ce543c104654ee03c848d7e3d7b357a
Nathan <nadarrah15@gmail.com>
2016-09-28 16:11:51 -0400

    GAHD Y

--- Exact Sum 11057/src/Main.java
+++ Exact Sum 11057/src/Main.java
@@ -3,74 +3,62 @@ import java.util.Scanner;
 
 public class Main {
 
-	static int M;
-
 	public static void main(String[] args) {
-	
 		Scanner reader = new Scanner(System.in);
+		
 		while(reader.hasNext()){
-			
-			int N = reader.nextInt();
+			int N = Integer.parseInt(reader.next());
 			int[] array = new int[N];
 			
 			for(int i = 0; i < N; i++){
-				array[i] = reader.nextInt();
+				array[i] = Integer.parseInt(reader.next());
 			}
 			
-			M = reader.nextInt();
+			int M = Integer.parseInt(reader.next());
 			reader.nextLine();
 			reader.nextLine();
-			int answerOne = 0;
-			int answerTwo = 0;
-			int i;
+			
 			Arrays.sort(array);
-			if(N % 2 == 0){
-				i = N / 2;
-			}
-			else {
-				i = N / 2 + 1;
-			}
-			for(; i >= 0; i--){
-				answerOne = array[i];
-				answerTwo = binarySearch(array, i);
-				
-				if(answerTwo != 0 && answerTwo != -1) break;
+			
+			int x = 0;
+			int y = 0;
+			
+			int i = 0;
+			int j = N - 1;
+			
+			while(i < j){
+				if(array[i] + array[j] > M)
+					j--;
+				else if(array[i] + array[j] < M)
+					i++;
+				else if(array[i] + array[j] == M){
+					if(x == 0 && y == 0){
+						if(array[i] < array[j]){
+							x = array[i];
+							y = array[i];
+						}
+						else{
+							x = array[j];
+							y = array[i];
+						}
+					}
+					else{
+						if(array[i] > array[j] && array[i] - array[j] < y - x){
+							x = array[j];
+							y = array[i];
+						}
+						else if(array[i] < array[j] && array[j] - array[i] < y - x){
+							x = array[i];
+							y = array[i];
+						}
+					}
+					
+					i++;
+					j--;
+				}
 			}
 			
-			if(answerOne < answerTwo){
-				System.out.println("Peter should buy books whose prices are " + answerOne + " and " + answerTwo + ".");
-				System.out.println("");
-			}
-			else{
-				System.out.println("Peter should buy books whose prices are " + answerTwo + " and " + answerOne + ".");
-				System.out.println("");
-			}
+			System.out.println("Peter should buy books whose prices are " + x + " and " + y + ".\n");
 		}
-	}
-
-	public static int binarySearch(int[] array, int index) {
-		int low = 0;
-		int high = array.length - 1;
-		boolean incr = true;
-		while (low <= high) {
-			int mid = (low + high) / 2;
-			if(M - array[index] == array[mid] && mid != index) {
-				return array[mid];
-			} 
-			else if (M - array[index] > array[mid]){
-				low = mid + 1;
-				incr = true;
-			}
-			else if (M - array[index] < array[mid]){
-				high = mid - 1;
-				incr = false;
-			}
-			else{
-				if(incr) low++;
-				else high--;
-			}
-		}
-
-		return -1;
 	}
 }
