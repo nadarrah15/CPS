17a37506f27fa4bdbe87b81919687ae2231b3e16
Nathan <nadarrah15@gmail.com>
2016-09-29 11:39:25 -0400

    y

--- Generics/src/Main.java
+++ Generics/src/Main.java
@@ -9,16 +9,26 @@ public class Main {
 		strings.add(5);
 		//we could create a new vector for strings only but that would be extensive. Especially if we wanted that for every data type
 		
-		//throws a runtime error because the objects cannot be converted
-		String str = (String) strings.get(1);
-		stringMethod(str);
+		try{
+			//throws a runtime error because the objects cannot be converted
+			String str = (String) strings.get(1);
+			stringMethod(str);
+		} catch (Exception e){
+			System.out.println(e);
+		}
 		
 		structure5.Vector<String> genString = new structure5.Vector<String>();
 		genString.add("Bubba");
 		
-		genString.add("Bubba");
-		//genString.add(5); throws a compilation error because add(int) is not defined with generics
+		try{
+			genString.add("Bubba");
+			genString.add(5); //throws a compilation error because add(int) is not defined with generics
+		} catch(Exception e){
+			System.out.println(e);
+		}
 		
+		
+		stringMethod(genString.get(0));
 	}
 	
 	static void stringMethod(String str){
