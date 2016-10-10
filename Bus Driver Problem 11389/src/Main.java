d415d65a075d75f228e7993128a322cd467168f8
nmd001 <nmd001@lyn188-03>
2016-10-10 14:54:53 -0400

    yay

--- Bus Driver Problem 11389/src/Main.java
+++ Bus Driver Problem 11389/src/Main.java
@@ -1,9 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner reader = new Scanner(System.in);
+		
+		while(true){
+			int n = reader.nextInt();
+			int d = reader.nextInt();
+			int r = reader.nextInt();
+			
+			if(n == 0 && d == 0 && r == 0) break;
+			
+			int[] day = new int[n];
+			for(int i = 0; i < n; i++){
+				day[i] = reader.nextInt();
+			}
+			reader.nextLine();
+			
+			int[] evening = new int[n];
+			for(int i = 0; i < n; i++){
+				evening[i] = reader.nextInt();
+			}
+			reader.nextLine();
+			
+			Arrays.sort(day);
+			Arrays.sort(evening);
+			
+			int sum = 0;
+			for(int i = 0; i < n; i++){
+				int temp = day[i] + evening[evening.length - i - 1] - d;
+				if(temp > 0) sum += temp;
+			}
+			
+			System.out.println(sum * r);
+		}
 	}
 
 }
