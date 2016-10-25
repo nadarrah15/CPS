e3eb352edd9d67529e1a35e4533a76aee5af96b4
nmd001 <nmd001@lyn188-07>
2016-10-25 13:51:27 -0400

    in progress

--- Beverages - 11060/src/Main.java
+++ Beverages - 11060/src/Main.java
@@ -1,9 +1,50 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
+	static class Vertex{
+		String name;
+		ArrayList<Edge> edges = new ArrayList<Edge>();
+		
+		public Vertex(String name){
+			this.name = name;
+		}
+	}
+	
+	static class Edge{
+		String dest;
+		
+		public Edge(String dest){
+			this.dest = dest;
+		}
+	}
+	
+	static ArrayList<Vertex> graph;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner reader = new Scanner(System.in);
+		
+		int count = 0;
+		while(reader.hasNext()){
+			count++;
+			graph = new ArrayList<Vertex>();
+			
+			int N = reader.nextInt();
+			reader.nextLine();
+			for(int i = 0; i < N; i++){
+				String str = reader.nextLine();
+				graph.add(new Vertex(str));
+			}
+			
+			int M = reader.nextInt();
+			reader.nextLine();
+			for(int i = 0; i < M; i++){
+				String b1 = reader.nextLine();
+				String b2 = reader.nextLine();
+				graph.get(graph.indexOf(new Vertex(b1))).edges.add(new Edge(b2));
+			}
+		}
 	}
 
 }
