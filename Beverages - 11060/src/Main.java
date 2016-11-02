bfeed991545d7f9780163bc226143956f1a8b2df
nmd001 <nmd001@lyn188-16>
2016-11-02 16:53:01 -0400

    update

--- Beverages - 11060/src/Main.java
+++ Beverages - 11060/src/Main.java
@@ -1,19 +1,30 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 	
-	static class Beverage{
-		String name;
-		ArrayList<String> dependent = new ArrayList<String>();
+	static class Edge {
+		int dest;
 		
-		public Beverage(String name){
-			this.name = name;
+		Edge(int d) {
+			dest = d;
 		}
 	}
 	
-	static ArrayList<Beverage> bev;
-	static ArrayList<String> names;
+	static class Vertex {
+		ArrayList<Edge> edges;
+		int name;
+		
+		Vertex(int n) {
+			name = n;
+			edges = new ArrayList<Edge>();
+		}
+	}
+	
+	static ArrayList<String> bev;
+	static ArrayList<Vertex> graph;
 	
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
@@ -22,16 +33,55 @@ public class Main {
 			count++;
 			int N = reader.nextInt();
 			reader.nextLine();
-			bev = new ArrayList<Beverage>();
+			bev = new ArrayList<String>();
+			graph = new ArrayList<Vertex>();
 			
 			for(int i = 0; i < N; i++){
-				bev.add(new Beverage(reader.nextLine()));
+				bev.add(reader.nextLine());
+				graph.add(new Vertex(i));
 			}
+			
+			int[] indeg = new int[bev.size()];
 			
 			int M = reader.nextInt();
 			reader.nextLine();
 			for(int i = 0; i < M; i++){
+				int a = bev.indexOf(reader.next()), b = bev.indexOf(reader.next());
+				ArrayList<Edge> edges = graph.get(i).edges;
+				edges.add(new Edge(b));
+			}
+			
+			for (int i=0; i<graph.size(); ++i) {
+				ArrayList<Edge> edges = graph.get(i).edges;
 				
+				for (int j = 0; j < edges.size(); ++j) {
+					++indeg[edges.get(j).dest];
+				}
+			}
+			
+			Queue<Integer> q = new LinkedList<Integer>();
+			
+			for(int i = 0; i < indeg.length; i++)
+				if (indeg[i] == 0)
+					q.add(i);
+			
+			System.out.print("Case # "+ count + ": Dilbert should drink beverages in this order:");
+			
+			while (!q.isEmpty()) {
+				// find a vertex with in-degree 0
+				int v = q.remove();
+				
+				// add it to the list
+				System.out.print(" " + bev.get(v));
+				
+				// decrease the in-degrees of v's neighbors
+				ArrayList<Edge> edges = graph.get(v).edges;
+				for (int i = 0; i < edges.size(); i++) {
+					int w = edges.get(i).dest;
+					--indeg[w];
+					if (indeg[w] == 0)
+						q.add(w);
+				}
 			}
 		}
 	}
