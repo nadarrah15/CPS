f24640fb18c2ab2414b6b21e70f250b9dcf12d64
nmd001 <nmd001@lyn188-07>
2016-11-08 13:39:37 -0500

    update

--- Sending Email - 10986/src/Main.java
+++ Sending Email - 10986/src/Main.java
@@ -1,9 +1,49 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	static class Edge {
+		int dest;
+		int weight;
+		
+		Edge(int d) {
+			dest = d;
+			weight = 1;
+		}
+		
+		Edge(int d, int w) {
+			dest = d;
+			weight = w;
+		}
 	}
-
+	
+	static class Vertex {
+		ArrayList<Edge> edges;
+		int name;
+		
+		Vertex(int n) {
+			name = n;
+			edges = new ArrayList<Edge>();
+		}
+	}
+	
+	static ArrayList<Vertex> graph;
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		
+		for(int Case = 1; Case <= N; Case++){
+			int n = in.nextInt(), m = in.nextInt(), S = in.nextInt(), T = in.nextInt();
+			
+			
+			
+			for(int i = 0; i < m; i++){
+				int a = in.nextInt(), b = in.nextInt(), w = in.nextInt();
+				
+			}
+		}
+	}
 }
